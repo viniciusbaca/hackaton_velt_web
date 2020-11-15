dd20ace465c4a76acfe25393d85a85fdd5cee36c
Baca <viniciusfbaca@usp.br>
2020-11-15 15:27:04 -0300

    refactor and clean

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Screens/homeScreen.dart';
+
 void main() {
-  return runApp(MaterialApp(
-      theme: ThemeData(fontFamily: 'Raleway'),
-      home: HomeScreen()));
+  return runApp(MaterialApp(theme: ThemeData(fontFamily: 'LibreBaskerville'), home: HomeScreen()));
 }
